c5a751f50a3741187506b43a2ece1ff5ac747a04
MacBook <macbook@MacBooks-MacBook-Pro-2.local>
2023-02-05 14:50:57 +0400

    final solution

--- tests/pr1_test.py
+++ tests/pr1_test.py
@@ -1,4 +1,6 @@
 import pytest
+import numpy as np
+from normal_equation import linear_regression
 # import numpy as np
 # from normal_equation import linear_regression
 
@@ -6,10 +8,16 @@ import pytest
 
 # def test_linear_regression_normal_equation(X, y, expected_w):
 #     assert np.allclose(linear_regression(X, y), expected_w, rtol=1e-2, atol=1e-2)
+@pytest.mark.parametrize("X, y, expected_w", [
+    (np.array([[1, 1, 1, 1], [2104, 1416, 1534, 852], [5, 3, 3, 2], [1, 2, 2, 1], [45, 41, 30, 36]]).T,
+     np.array([460, 232, 315, 178]), np.array([-7.21308594e+01, 5.35469055e-02, 2.16058594e+02, -6.91113281e+00,
+                                               -7.29681396e+00])),
+
+    (np.array([[1, 1], [1, 6]]), np.array([1, 2]), np.array([0.8, 0.2])),
+
+])
 
 
-import numpy as np
-from normal_equation import linear_regression
 
 
 def test_linear_regression_normal_equation():
@@ -27,11 +35,7 @@ def test_linear_regression_normal_equation_2():
     assert np.allclose(linear_regression(X, y), expected_w, rtol=1e-2, atol=1e-2) 
 
 
-@pytest.mark.parametrize("X, y, expected_w", [
-    (np.array([[1, 1, 1, 1], [2104, 1416, 1534, 852], [5, 3, 3, 2], [1, 2, 2, 1], [45, 41, 30, 36]]).T,
-     np.array([460, 232, 315, 178]), np.array([-7.21308594e+01, 5.35469055e-02, 2.16058594e+02, -6.91113281e+00,
-                                               -7.29681396e+00])),
 
-    (np.array([[1, 1], [1, 6]]), np.array([1, 2]), np.array([0.8, 0.2])),
 
-])
+
+
